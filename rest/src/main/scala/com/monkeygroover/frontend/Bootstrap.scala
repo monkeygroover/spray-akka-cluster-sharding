240bde0ed4ce0460e43b322b6b35236ec43a9101
monkeygroover <rich@monkeygroover.com>
2015-10-18 00:17:21 +0100

    use Akka-HTTP instead of spray

--- rest/src/main/scala/com/monkeygroover/frontend/Bootstrap.scala
+++ rest/src/main/scala/com/monkeygroover/frontend/Bootstrap.scala
@@ -1,31 +1,31 @@
 package com.monkeygroover.frontend
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.ActorSystem
 import akka.cluster.sharding.ClusterSharding
-import akka.io.IO
+import akka.http.scaladsl.Http
+import akka.stream.ActorMaterializer
 import com.monkeygroover.service.CustomerService
 import com.typesafe.config.ConfigFactory
-import spray.can.Http
 
 /**
  * Created by monkeygroover on 09/10/15.
  */
-object Bootstrap extends App {
+object Bootstrap extends RestRoutes {
   val conf = ConfigFactory.load()
 
   implicit val system = ActorSystem("ClusterSystem", conf)
+  import system.dispatcher
+  implicit val materializer = ActorMaterializer()
 
   // register cluster sharding, but this node is proxy only (i.e. doesn't host actor instances, has a None props)
-  val customerRegion = ClusterSharding(system).startProxy(
+  val shardRegion = ClusterSharding(system).startProxy(
     typeName = CustomerService.Shard.name,
     None,
     CustomerService.Shard.entityIdExtractor,
     CustomerService.Shard.shardIdExtractor
   )
 
-  //create our service actor
-  val service = system.actorOf(Props(classOf[RestActor], customerRegion), "my-service")
-
-  //bind our actor to an HTTP port
-  IO(Http) ! Http.Bind(service, interface = "localhost", port = 8080)
+  def main(args: Array[String]): Unit = {
+    Http().bindAndHandle(handler = route, interface = "localhost", port = 8080)
+  }
 }
