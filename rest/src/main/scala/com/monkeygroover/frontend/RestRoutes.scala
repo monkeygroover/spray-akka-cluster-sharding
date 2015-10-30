4137f988fa0705def83af51526b7b92667df3d31
monkeygroover <rich@monkeygroover.com>
2015-10-30 23:43:23 +0000

    Working Docker

--- rest/src/main/scala/com/monkeygroover/frontend/RestRoutes.scala
+++ rest/src/main/scala/com/monkeygroover/frontend/RestRoutes.scala
@@ -3,6 +3,7 @@ package com.monkeygroover.frontend
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.server.{Directives, Route}
+import akka.http.scaladsl.model.StatusCodes
 import akka.stream.Materializer
 import com.monkeygroover.commands._
 
@@ -11,7 +12,9 @@ import com.monkeygroover.commands._
  */
 abstract class RestRoutes(implicit system: ActorSystem, materializer: Materializer) extends Directives {
 
-  lazy val routes = addData ~ updateData ~ deleteData ~ getData ~ getHistory
+  lazy val routes = ping ~ addData ~ updateData ~ deleteData ~ getData ~ getHistory
+
+  val ping = path("ping") & complete(StatusCodes.OK)
 
   val addData =
     post {
